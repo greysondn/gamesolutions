6ab04b1b9433185ab4a24eed02b6c7a7ecdeb208
J. Dorian Greyson L <greysondn@gmail.com>
2020-03-29 19:57:19 +0000

    Solved it?

--- mattparkermp/20200325_spin_the_table/toolkit.py
+++ mattparkermp/20200325_spin_the_table/toolkit.py
@@ -1,9 +1,14 @@
 def main():
-    foo = [1, 2, 3, 4, 5, 6]
+    table =  [1, 2, 3, 4, 5, 6]
+    people = [6, 5, 4, 3, 2, 1]
 
-    print(f"start:     {foo}")
-    print(f"forwards:  {rotForwards(foo)}")
-    print(f"backwards: {rotBackwards(foo)}")
+    print(f"start table:  {table}")
+    print(f"start people: {people}")
+    print("")
+    swp = getMaxArrangement(table, people)
+    print(f"max table:  {swp[0]}")
+    print(f"max people: {swp[1]}")
+    print(f"max count:  {swp[2]}")
 
 def rotBackwards(someList):
     ret = []
@@ -17,6 +22,35 @@ def rotForwards(someList):
     ret.append(someList[-1])
     for i in range(len(someList)-1):
         ret.append(someList[i])
+    return ret
+
+def getMaxArrangement(table, people):
+    ret = None
+    retArrangement = table
+    retPeople      = people
+    retCount       = countMatches(table, people)
+
+    curPeople = people
+
+    for i in range(len(table)):
+        curPeople = rotForwards(curPeople)
+        curCount  = countMatches(table, curPeople)
+
+        if (curCount > retCount):
+            retCount  = curCount
+            retPeople = curPeople
+        
+    ret = (retArrangement, retPeople, retCount)
+    return ret
+
+def countMatches(table, people):
+    # a running count of matches
+    ret = 0
+    
+    for i in range(len(table)):
+        if (table[i] == people[i]):
+            ret += 1
+
     return ret
 
 if __name__ == "__main__":
