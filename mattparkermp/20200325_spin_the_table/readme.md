225737c5f5d2079dcc0938d5c696624f2efb6458
J. Dorian Greyson L <greysondn@gmail.com>
2020-03-29 21:45:11 +0000

    well, it's the tools to solve Parker's second problem, anyway.

--- mattparkermp/20200325_spin_the_table/readme.md
+++ mattparkermp/20200325_spin_the_table/readme.md
@@ -1,6 +1,6 @@
 Given a table of 7 people, find a configuration where you can set number packets
-in order on the table and yet not have any matches to people sitting at seats by
-rotation of the table itself.
+in order on the table and yet not have more than 1 match to people sitting at
+seats by rotation of the table itself.
 
 Imagine the table as a list. Imagine the place settings as a list. If they both
 matched perfectly, it would look like this:
@@ -11,3 +11,19 @@ People: 0, 1, 2, 3, 4, 5, 6
 ```
 The people cannot be rotated, and the people may sit in random order. The table
 can be rotated, however.
+
+--------------------------------------------------------------------------------
+
+Parker offers an open problem, which is to describe a formula for the worst case
+given some number of people at the correct seats to start at tables of arbitrary
+sizes.
+
+```
+Table 1
+Must always match
+
+Table 2
+If it starts with any matches, it must have 2 matches
+
+Table 3
+
--- mattparkermp/20200325_spin_the_table/toolkit.py
+++ mattparkermp/20200325_spin_the_table/toolkit.py
@@ -1,14 +1,86 @@
 def main():
-    table =  [1, 2, 3, 4, 5, 6]
-    people = [6, 5, 4, 3, 2, 1]
+    startCorrect = 15
+    seatCount    = 20
+    
+    # build table:
+    table = []
+    for i in range(seatCount):
+        table.append(i)
 
     print(f"start table:  {table}")
-    print(f"start people: {people}")
     print("")
-    swp = getMaxArrangement(table, people)
-    print(f"max table:  {swp[0]}")
-    print(f"max people: {swp[1]}")
-    print(f"max count:  {swp[2]}")
+    swp = buildAndTestTake2(startCorrect, table, table, [], ([], [], len(table) + 1))
+    # swp = buildAndTestSeatArrangements(startCorrect, table, table, [], [], len(table) + 1, "")
+    print(f"{swp}")
+
+
+def buildAndTestTake2(startCorrect, table, remaining, current, worst):
+    retWorst = (worst[0].copy(), worst[1].copy(), worst[2])
+    
+    # limit check
+    if (0 == len(remaining)):
+        # limit function
+        if (countMatches(table, current) == startCorrect):
+                curMax = getMaxArrangement(table, current)
+                if (curMax[2] < retWorst[2]):
+                    retWorst = (curMax[0].copy(), curMax[1].copy(), curMax[2])
+            
+    else:
+        # recursive function
+        
+        # remaining into current
+        for i in range(len(remaining)):
+            curChop = current.copy()
+            remChop = remaining.copy()
+            curChop.append(remChop.pop(i))
+
+            # pump downwards
+            retWorst = buildAndTestTake2(startCorrect, table, remChop, curChop, retWorst)
+    return retWorst
+
+
+def buildAndTestSeatArrangements(startCorrect, table, remaining, current, worst, worstCount, prefix):
+    print(f"{prefix}- START -")
+    print(f"{prefix}INPUT: startCorrect: {startCorrect}")
+    print(f"{prefix}INPUT: table: {table}")
+    print(f"{prefix}INPUT: remaining: {remaining}")
+    print(f"{prefix}INPUT: current: {current}")
+    print(f"{prefix}INPUT: worst: {worst}")
+    print(f"{prefix}INPUT: worstCount: {worstCount}")
+    retWorst      = worst
+    retWorstCount = worstCount
+
+    print(f"{prefix}DEBUG: LIMIT CHECK FORK")
+    if (len(remaining) == 0):
+        print(f"{prefix}DEBUG: LIMIT")
+        # limit step
+        # make sure we have the correct number of starting matches
+        if (countMatches(table, current) == startCorrect):
+            # see if we've gotten a worse arrangement
+            curMax = getMaxArrangement(table, current)
+            mCount = curMax[2]
+
+            if (mCount < retWorstCount):
+                print(f"{prefix}INFO :found worse!")
+                retWorstCount = mCount
+                retWorst = (curMax[0], curMax[1])
+                print(f"{prefix}INF0 : m    : {mCount}")
+                print(f"{prefix}INFO : r    : {retWorstCount}")
+                print(f"{prefix}INFO : cMax : {curMax}")
+                print(f"{prefix}INFO : ret  : {retWorst}")
+    else:
+        # recursive step
+        print(f"{prefix}DEBUG: RECURSIVE")
+        for i in range(len(remaining)):
+            remSwp     = remaining.copy()
+            curSwp     = current.copy()
+            curSwp.append(remSwp.pop(i))
+            retWorst, retWorstCount = buildAndTestSeatArrangements(startCorrect, table, remSwp, curSwp, worst, worstCount, (prefix + "    "))
+        print(f" INFO: return planned: {retWorstCount} {retWorst}")
+    print(f"INFO: returning: {retWorstCount} {retWorst}")
+    print(f"{prefix}- RETURN -")
+    return retWorst, retWorstCount
+    
 
 def rotBackwards(someList):
     ret = []
@@ -26,11 +98,11 @@ def rotForwards(someList):
 
 def getMaxArrangement(table, people):
     ret = None
-    retArrangement = table
-    retPeople      = people
+    retArrangement = table.copy()
+    retPeople      = people.copy()
     retCount       = countMatches(table, people)
 
-    curPeople = people
+    curPeople = people.copy()
 
     for i in range(len(table)):
         curPeople = rotForwards(curPeople)
@@ -38,7 +110,7 @@ def getMaxArrangement(table, people):
 
         if (curCount > retCount):
             retCount  = curCount
-            retPeople = curPeople
+            retPeople = curPeople.copy()
         
     ret = (retArrangement, retPeople, retCount)
     return ret
