812964d8c0aa6209710e9f7134fc4f11f7a91b6c
J. Dorian Greyson L <greysondn@gmail.com>
2020-06-16 08:04:52 -0400

    TQ JS - header file to avoid repeated boilerplate.

---
+++ twilioquest/javascript/header.js
@@ -0,0 +1,9 @@
+// TwilioQuest 3.1.26
+// Works in:
+//   - 3.1.26
+//
+// Fun fact: I walk into this good at Python, but terrible at JS.
+// Let's see if basic competency can be established just using
+// TwilioQuest and what I already know from Python/C++
+//
+
