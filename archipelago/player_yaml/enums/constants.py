c392f0c2e27f85fbd75f61ca5f662d2d1530db66
J. 'Dorian Greyson' L <greysondn@gmail.com>
2023-11-11 12:08:23 -0500

    extract enum related constants to new file

---
+++ archipelago/player_yaml/enums/constants.py
@@ -0,0 +1,37 @@
+from .game    import Game
+from .genre   import Genre
+from .machine import Machine
+
+MACHINES_TO_GAMES:dict[Machine, int] = {
+    Machine.STORM_TOWER     :   Game.DOOM_1993.value + 
+                                Game.RISK_OF_RAIN_2.value + 
+                                Game.ROGUE_LEGACY.value +
+                                Game.SONIC_ADVENTURE_2_BATTLE.value +
+                                Game.STARDEW_VALLEY.value +
+                                Game.SUPER_MARIO_WORLD.value,
+                                
+    Machine.URSINE_LAPTOP   :   Game.DOOM_1993.value +
+                                Game.SUPER_MARIO_WORLD.value,
+}
+
+GAMES_TO_GENRES:dict[Game, int] = {
+    Game.DOOM_1993 :    Genre.IMPLEMENTED.value +
+                        Genre.FIRST_PERSON_SHOOTER.value +
+                        Genre.SHOOTER.value,
+    
+    Game.RISK_OF_RAIN_2 :   Genre.ROGUELITE.value +
+                            Genre.SHOOTER.value +
+                            Genre.THIRD_PERSON_SHOOTER.value,
+    
+    Game.ROGUE_LEGACY : Genre.PLATFORMER.value +
+                        Genre.ROGUELITE.value,
+    
+    Game.SONIC_ADVENTURE_2_BATTLE : Genre.PLATFORMER.value,
+    
+    Game.STARDEW_VALLEY :   Genre.FARMING_SIMULATOR.value +
+                            Genre.ROGUELITE.value +
+                            Genre.RPG.value,
+    
+    Game.SUPER_MARIO_WORLD :    Genre.IMPLEMENTED.value +
+                                Genre.PLATFORMER.value,
+}
--- archipelago/player_yaml/main.py
+++ archipelago/player_yaml/main.py
@@ -10,43 +10,10 @@ from enums.difficulty import Difficulty
 from enums.game import Game
 from enums.genre import Genre
 from enums.machine import Machine
+from enums.constants import GAMES_TO_GENRES, MACHINES_TO_GAMES
 
 # This is just a quick and dirty python script to manage my archipelago settings
 # because the conditionals got too weird over time.
-    
-MACHINES_TO_GAMES:dict[Machine, int] = {
-    Machine.STORM_TOWER     :   Game.DOOM_1993.value + 
-                                Game.RISK_OF_RAIN_2.value + 
-                                Game.ROGUE_LEGACY.value +
-                                Game.SONIC_ADVENTURE_2_BATTLE.value +
-                                Game.STARDEW_VALLEY.value +
-                                Game.SUPER_MARIO_WORLD.value,
-                                
-    Machine.URSINE_LAPTOP   :   Game.DOOM_1993.value +
-                                Game.SUPER_MARIO_WORLD.value,
-}
-
-GAMES_TO_GENRES:dict[Game, int] = {
-    Game.DOOM_1993 :    Genre.IMPLEMENTED.value +
-                        Genre.FIRST_PERSON_SHOOTER.value +
-                        Genre.SHOOTER.value,
-    
-    Game.RISK_OF_RAIN_2 :   Genre.ROGUELITE.value +
-                            Genre.SHOOTER.value +
-                            Genre.THIRD_PERSON_SHOOTER.value,
-    
-    Game.ROGUE_LEGACY : Genre.PLATFORMER.value +
-                        Genre.ROGUELITE.value,
-    
-    Game.SONIC_ADVENTURE_2_BATTLE : Genre.PLATFORMER.value,
-    
-    Game.STARDEW_VALLEY :   Genre.FARMING_SIMULATOR.value +
-                            Genre.ROGUELITE.value +
-                            Genre.RPG.value,
-    
-    Game.SUPER_MARIO_WORLD :    Genre.IMPLEMENTED.value +
-                                Genre.PLATFORMER.value,
-}
 
 class ApConfig():
     def __init__(self):
