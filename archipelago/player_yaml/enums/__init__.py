25a6fd855c074b991aa84e8692be4b92c7f89e19
J. 'Dorian Greyson' L <greysondn@gmail.com>
2023-11-11 12:16:49 -0500

    fix import errors

--- archipelago/player_yaml/enums/__init__.py
+++ archipelago/player_yaml/enums/__init__.py
@@ -1,14 +1,14 @@
-import constants
+from . import constants
 GAMES_TO_GENRES   = constants.GAMES_TO_GENRES
 MACHINES_TO_GAMES = constants.MACHINES_TO_GAMES
 
-from difficulty import Difficulty
+from .difficulty import Difficulty
 
-import game
+from . import game
 Game = game.Game
 
-import genre
+from . import genre
 Genre = genre.Genre
 
-import machine
+from . import machine
 Machine = machine.Machine
