3c0c2fc35b586627e65670eb7a3426f166c9810e
J. 'Dorian Greyson' L <greysondn@gmail.com>
2024-06-05 13:02:36 -0400

    update SMW to AP 0.4.6

--- archipelago/player_yaml/config/smw.py
+++ archipelago/player_yaml/config/smw.py
@@ -21,7 +21,7 @@ class SuperMarioWorldBase(ApConfig):
         # reconfigure non-difficulty parts of base class
         self.game = Game.SUPER_MARIO_WORLD
         self.apgame = "Super Mario World"
-        self.requires_version = "0.4.4"
+        self.requires_version = "0.4.6"
         
         self.progression_balancing.set(50)
 
@@ -37,14 +37,26 @@ class SuperMarioWorldBase(ApConfig):
         self.bosses_required:IntComp = IntComp("bosses_required", 7, 4)
         self.components.append(self.bosses_required)
         
-        self.number_of_yoshi_eggs:IntComp = IntComp("number_of_yoshi_eggs", 50, 4)
-        self.components.append(self.number_of_yoshi_eggs)
+        self.max_yoshi_egg_cap:IntComp = IntComp("max_yoshi_egg_cap", 50, 4)
+        self.components.append(self.max_yoshi_egg_cap)
         
         self.percentage_of_yoshi_eggs:IntComp = IntComp("percentage_of_yoshi_eggs", 100, 4)
         self.components.append(self.percentage_of_yoshi_eggs)
         
         self.dragon_coin_checks:BoolComp = BoolComp("dragon_coin_checks", False, 4)
         self.components.append(self.dragon_coin_checks)
+        
+        self.moon_checks:BoolComp = BoolComp("moon_checks", False, 4)
+        self.components.append(self.moon_checks)
+        
+        self.hidden_1up_checks:BoolComp = BoolComp("hidden_1up_checks", False, 4)
+        self.components.append(self.hidden_1up_checks)
+        
+        self.bonus_block_checks:BoolComp = BoolComp("bonus_block_checks", False, 4)
+        self.components.append(self.bonus_block_checks)
+        
+        self.blocksanity:BoolComp = BoolComp("blocksanity", False, 4)
+        self.components.append(self.blocksanity)
         
         self.bowser_castle_doors:StrComp = StrComp("bowser_castle_doors", "vanilla", 4)
         self.components.append(self.bowser_castle_doors)
@@ -64,8 +76,11 @@ class SuperMarioWorldBase(ApConfig):
         self.swap_donut_gh_exits:BoolComp = BoolComp("swap_donut_gh_exits", False, 4)
         self.components.append(self.swap_donut_gh_exits)
         
-        self.display_received_item_popups:StrComp = StrComp("display_received_item_popups", "progression", 4)
+        self.display_received_item_popups:StrComp = StrComp("display_received_item_popups", "progression_minus_yoshi_eggs", 4)
         self.components.append(self.display_received_item_popups)
+        
+        self.junk_fill_percentage:IntComp = IntComp("junk_fill_percentage", 0, 4)
+        self.components.append(self.junk_fill_percentage)
         
         self.trap_fill_percentage:IntComp = IntComp("trap_fill_percentage", 0, 4)
         self.components.append(self.trap_fill_percentage)
@@ -90,6 +105,12 @@ class SuperMarioWorldBase(ApConfig):
         self.timer_trap_weight:StrEnumComp = StrEnumComp("timer_trap_weight", "medium", _trapWeights, 4)
         self.components.append(self.timer_trap_weight)
         
+        self.reverse_trap_weight:StrEnumComp = StrEnumComp("reverse_trap_weight", "medium", _trapWeights, 4)
+        self.components.append(self.reverse_trap_weight)
+        
+        self.thwimp_trap_weight:StrEnumComp = StrEnumComp("thwimp_trap_weight", "medium", _trapWeights, 4)
+        self.components.append(self.thwimp_trap_weight)
+        
         self.autosave:BoolComp = BoolComp("autosave", True, 4)
         self.components.append(self.autosave)
         
@@ -113,6 +134,14 @@ class SuperMarioWorldBase(ApConfig):
         self.music_shuffle:StrEnumComp = StrEnumComp("music_shuffle", "none", _musicShuffleTypes, 4)
         self.components.append(self.music_shuffle)
         
+        _sfxShuffleTypes:list[str] = [
+            "none",
+            "full",
+            "singularity",
+        ]
+        self.sfx_shuffle:StrEnumComp = StrEnumComp("sfx_shuffle", "none", _sfxShuffleTypes, 4)
+        self.components.append(self.sfx_shuffle)
+                
         _marioPalettes:list[str] = [
             "mario",
             "luigi",
@@ -126,13 +155,16 @@ class SuperMarioWorldBase(ApConfig):
         self.mario_palette:StrEnumRandomizableComp = StrEnumRandomizableComp("mario_palette", "mario", _marioPalettes, 4)
         self.components.append(self.mario_palette)
         
-        self.foreground_palette_shuffle:BoolComp = BoolComp("foreground_palette_shuffle", False, 4)
-        self.components.append(self.foreground_palette_shuffle)
+        _paletteShuffleTypes:list[str] = [
+            "off",
+            "on_legacy",
+            "on_curated",
+        ]
         
-        self.background_palette_shuffle:BoolComp = BoolComp("background_palette_shuffle", False, 4)
-        self.components.append(self.background_palette_shuffle)
-        
-        self.overworld_palette_shuffle:BoolComp = BoolComp("overworld_palette_shuffle", False, 4)
+        self.level_palette_shuffle:StrEnumComp = StrEnumComp("level_palette_shuffle", "off", _paletteShuffleTypes, 4)
+        self.components.append(self.level_palette_shuffle)
+                
+        self.overworld_palette_shuffle:StrEnumComp = StrEnumComp("level_palette_shuffle", "off", _paletteShuffleTypes, 4)
         self.components.append(self.overworld_palette_shuffle)
         
         self.starting_life_count:IntComp = IntComp("starting_life_count", 5, 4)
@@ -161,29 +193,36 @@ class SuperMarioWorld(SuperMarioWorldBase):
             self.death_link.set(False)
             self.goal.set("yoshi_egg_hunt")
             self.bosses_required.set(0)
-            self.number_of_yoshi_eggs.set(80)
+            self.max_yoshi_egg_cap.set(80)
             self.percentage_of_yoshi_eggs.set(1)
             self.dragon_coin_checks.set(False)
+            self.moon_checks.set(False)
+            self.hidden_1up_checks.set(False)
+            self.bonus_block_checks.set(False)
+            self.blocksanity.set(False)
             self.bowser_castle_doors.set("fast")
             self.bowser_castle_rooms.set("vanilla")
             self.level_shuffle.set(False)
             self.exclude_special_zone.set(True)
             self.boss_shuffle.set("none")
             self.swap_donut_gh_exits.set(True)
-            self.display_received_item_popups.set("progression")
+            self.display_received_item_popups.set("progression_minus_yoshi_eggs")
+            self.junk_fill_percentage.set(0)
             self.trap_fill_percentage.set(0)
             self.ice_trap_weight.set("none")
             self.stun_trap_weight.set("none")
             self.literature_trap_weight.set("none")
             self.timer_trap_weight.set("none")
+            self.reverse_trap_weight.set("none")
+            self.thwimp_trap_weight.set("none")
             self.autosave.set(True)
             self.early_climb.set(False)
             self.overworld_speed.set("fast")
             self.music_shuffle.set("full")
+            self.sfx_shuffle.set("none")
             self.mario_palette.set("random")
-            self.foreground_palette_shuffle.set(True)
-            self.background_palette_shuffle.set(True)
-            self.overworld_palette_shuffle.set(True)
+            self.level_palette_shuffle.set("on_curated")
+            self.overworld_palette_shuffle.set("on_curated")
             self.starting_life_count.set(99)
 
 
@@ -222,29 +261,36 @@ class SuperMarioWorld(SuperMarioWorldBase):
             self.death_link.set(False)
             self.goal.set("yoshi_egg_hunt")
             self.bosses_required.set(0)
-            self.number_of_yoshi_eggs.set(80)
+            self.max_yoshi_egg_cap.set(80)
             self.percentage_of_yoshi_eggs.set(25) # 20 eggs
             self.dragon_coin_checks.set(True)
+            self.moon_checks.set(False)
+            self.hidden_1up_checks.set(False)
+            self.bonus_block_checks.set(False)
+            self.blocksanity.set(False)
             self.bowser_castle_doors.set("fast")
             self.bowser_castle_rooms.set("vanilla")
             self.level_shuffle.set(False)
             self.exclude_special_zone.set(True)
             self.boss_shuffle.set("none")
             self.swap_donut_gh_exits.set(True)
-            self.display_received_item_popups.set("progression")
+            self.display_received_item_popups.set("progression_minus_yoshi_eggs")
+            self.junk_fill_percentage.set(0)
             self.trap_fill_percentage.set(0)
             self.ice_trap_weight.set("none")
             self.stun_trap_weight.set("none")
             self.literature_trap_weight.set("none")
             self.timer_trap_weight.set("none")
+            self.reverse_trap_weight.set("none")
+            self.thwimp_trap_weight.set("none")
             self.autosave.set(True)
             self.early_climb.set(False)
             self.overworld_speed.set("fast")
             self.music_shuffle.set("full")
+            self.sfx_shuffle.set("none")
             self.mario_palette.set("random")
-            self.foreground_palette_shuffle.set(True)
-            self.background_palette_shuffle.set(True)
-            self.overworld_palette_shuffle.set(True)
+            self.level_palette_shuffle.set("on_curated")
+            self.overworld_palette_shuffle.set("on_curated")
             self.starting_life_count.set(50)
 
             self.requires_plando = "items"
@@ -276,29 +322,36 @@ class SuperMarioWorld(SuperMarioWorldBase):
             self.death_link.set(False)
             self.goal.set("yoshi_egg_hunt")
             self.bosses_required.set(0)
-            self.number_of_yoshi_eggs.set(80)
+            self.max_yoshi_egg_cap.set(80)
             self.percentage_of_yoshi_eggs.set(63) # 50 eggs
             self.dragon_coin_checks.set(True)
+            self.moon_checks.set(False)
+            self.hidden_1up_checks.set(False)
+            self.bonus_block_checks.set(False)
+            self.blocksanity.set(False)
             self.bowser_castle_doors.set("fast")
             self.bowser_castle_rooms.set("vanilla")
             self.level_shuffle.set(True)
             self.exclude_special_zone.set(True)
             self.boss_shuffle.set("full")
             self.swap_donut_gh_exits.set(True)
-            self.display_received_item_popups.set("progression")
+            self.display_received_item_popups.set("progression_minus_yoshi_eggs")
+            self.junk_fill_percentage.set(25)
             self.trap_fill_percentage.set(100)
             self.ice_trap_weight.set("medium")
             self.stun_trap_weight.set("none")
             self.literature_trap_weight.set("none")
             self.timer_trap_weight.set("none")
+            self.reverse_trap_weight.set("none")
+            self.thwimp_trap_weight.set("none")
             self.autosave.set(True)
             self.early_climb.set(False)
             self.overworld_speed.set("fast")
             self.music_shuffle.set("full")
+            self.sfx_shuffle.set("none")
             self.mario_palette.set("random")
-            self.foreground_palette_shuffle.set(True)
-            self.background_palette_shuffle.set(True)
-            self.overworld_palette_shuffle.set(True)
+            self.level_palette_shuffle.set("on_curated")
+            self.overworld_palette_shuffle.set("on_curated")
             self.starting_life_count.set(50)
 
             self.requires_plando = "items"
@@ -332,29 +385,36 @@ class SuperMarioWorld(SuperMarioWorldBase):
             self.death_link.set(False)
             self.goal.set("yoshi_egg_hunt")
             self.bosses_required.set(0)
-            self.number_of_yoshi_eggs.set(80)
+            self.max_yoshi_egg_cap.set(80)
             self.percentage_of_yoshi_eggs.set(80) # 60 eggs
             self.dragon_coin_checks.set(True)
+            self.moon_checks.set(False)
+            self.hidden_1up_checks.set(False)
+            self.bonus_block_checks.set(False)
+            self.blocksanity.set(False)
             self.bowser_castle_doors.set("fast")
             self.bowser_castle_rooms.set("vanilla")
             self.level_shuffle.set(True)
             self.exclude_special_zone.set(True)
             self.boss_shuffle.set("full")
             self.swap_donut_gh_exits.set(True)
-            self.display_received_item_popups.set("progression")
+            self.display_received_item_popups.set("progression_minus_yoshi_eggs")
+            self.junk_fill_percentage.set(50)
             self.trap_fill_percentage.set(100)
             self.ice_trap_weight.set("medium")
             self.stun_trap_weight.set("medium")
             self.literature_trap_weight.set("none")
             self.timer_trap_weight.set("none")
+            self.reverse_trap_weight.set("none")
+            self.thwimp_trap_weight.set("none")
             self.autosave.set(True)
             self.early_climb.set(False)
             self.overworld_speed.set("fast")
             self.music_shuffle.set("full")
+            self.sfx_shuffle.set("none")
             self.mario_palette.set("random")
-            self.foreground_palette_shuffle.set(True)
-            self.background_palette_shuffle.set(True)
-            self.overworld_palette_shuffle.set(True)
+            self.level_palette_shuffle.set("on_curated")
+            self.overworld_palette_shuffle.set("on_curated")
             self.starting_life_count.set(20)
 
             self.requires_plando = "items"
@@ -388,29 +448,36 @@ class SuperMarioWorld(SuperMarioWorldBase):
             self.death_link.set(False)
             self.goal.set("yoshi_egg_hunt")
             self.bosses_required.set(0)
-            self.number_of_yoshi_eggs.set(80)
+            self.max_yoshi_egg_cap.set(80)
             self.percentage_of_yoshi_eggs.set(100) # 80 eggs
             self.dragon_coin_checks.set(True)
+            self.moon_checks.set(False)
+            self.hidden_1up_checks.set(False)
+            self.bonus_block_checks.set(False)
+            self.blocksanity.set(False)
             self.bowser_castle_doors.set("fast")
             self.bowser_castle_rooms.set("vanilla")
             self.level_shuffle.set(True)
             self.exclude_special_zone.set(True)
             self.boss_shuffle.set("full")
             self.swap_donut_gh_exits.set(True)
-            self.display_received_item_popups.set("progression")
+            self.display_received_item_popups.set("progression_minus_yoshi_eggs")
+            self.junk_fill_percentage.set(75)
             self.trap_fill_percentage.set(100)
             self.ice_trap_weight.set("medium")
             self.stun_trap_weight.set("medium")
             self.literature_trap_weight.set("medium")
             self.timer_trap_weight.set("none")
+            self.reverse_trap_weight.set("none")
+            self.thwimp_trap_weight.set("none")
             self.autosave.set(True)
             self.early_climb.set(False)
             self.overworld_speed.set("fast")
             self.music_shuffle.set("full")
+            self.sfx_shuffle.set("none")
             self.mario_palette.set("random")
-            self.foreground_palette_shuffle.set(True)
-            self.background_palette_shuffle.set(True)
-            self.overworld_palette_shuffle.set(True)
+            self.level_palette_shuffle.set("on_curated")
+            self.overworld_palette_shuffle.set("on_curated")
             self.starting_life_count.set(10)
 
             self.requires_plando = "items"
@@ -444,29 +511,36 @@ class SuperMarioWorld(SuperMarioWorldBase):
             self.death_link.set(False)
             self.goal.set("yoshi_egg_hunt")
             self.bosses_required.set(0)
-            self.number_of_yoshi_eggs.set(80)
+            self.max_yoshi_egg_cap.set(80)
             self.percentage_of_yoshi_eggs.set(100) # 80 eggs
             self.dragon_coin_checks.set(True)
+            self.moon_checks.set(False)
+            self.hidden_1up_checks.set(False)
+            self.bonus_block_checks.set(False)
+            self.blocksanity.set(False)
             self.bowser_castle_doors.set("fast")
             self.bowser_castle_rooms.set("vanilla")
             self.level_shuffle.set(True)
             self.exclude_special_zone.set(False)
             self.boss_shuffle.set("full")
             self.swap_donut_gh_exits.set(True)
-            self.display_received_item_popups.set("progression")
+            self.display_received_item_popups.set("progression_minus_yoshi_eggs")
+            self.junk_fill_percentage.set(100)
             self.trap_fill_percentage.set(100)
             self.ice_trap_weight.set("medium")
             self.stun_trap_weight.set("medium")
             self.literature_trap_weight.set("medium")
             self.timer_trap_weight.set("medium")
+            self.reverse_trap_weight.set("none")
+            self.thwimp_trap_weight.set("none")
             self.autosave.set(True)
             self.early_climb.set(False)
             self.overworld_speed.set("fast")
             self.music_shuffle.set("full")
+            self.sfx_shuffle.set("none")
             self.mario_palette.set("random")
-            self.foreground_palette_shuffle.set(True)
-            self.background_palette_shuffle.set(True)
-            self.overworld_palette_shuffle.set(True)
+            self.level_palette_shuffle.set("on_curated")
+            self.overworld_palette_shuffle.set("on_curated")
             self.starting_life_count.set(1)
 
             self.requires_plando = "items"
@@ -500,9 +574,13 @@ class SuperMarioWorld(SuperMarioWorldBase):
             self.death_link.set(False)
             self.goal.set("yoshi_egg_hunt")
             self.bosses_required.set(0)
-            self.number_of_yoshi_eggs.set(80)
+            self.max_yoshi_egg_cap.set(80)
             self.percentage_of_yoshi_eggs.set(100) # 80 eggs
             self.dragon_coin_checks.set(True)
+            self.moon_checks.set(False)
+            self.hidden_1up_checks.set(False)
+            self.bonus_block_checks.set(False)
+            self.blocksanity.set(False)
             self.bowser_castle_doors.set("fast")
             self.bowser_castle_rooms.set("vanilla")
             self.level_shuffle.set(True)
@@ -510,19 +588,22 @@ class SuperMarioWorld(SuperMarioWorldBase):
             self.boss_shuffle.set("full")
             self.swap_donut_gh_exits.set(True)
             self.display_received_item_popups.set("all")
+            self.junk_fill_percentage.set(100)
             self.trap_fill_percentage.set(100)
             self.ice_trap_weight.set("none")
             self.stun_trap_weight.set("none")
             self.literature_trap_weight.set("medium")
             self.timer_trap_weight.set("medium")
+            self.reverse_trap_weight.set("none")
+            self.thwimp_trap_weight.set("none")
             self.autosave.set(True)
             self.early_climb.set(False)
             self.overworld_speed.set("slow")
             self.music_shuffle.set("full")
+            self.sfx_shuffle.set("none")
             self.mario_palette.set("random")
-            self.foreground_palette_shuffle.set(True)
-            self.background_palette_shuffle.set(True)
-            self.overworld_palette_shuffle.set(True)
+            self.level_palette_shuffle.set("on_curated")
+            self.overworld_palette_shuffle.set("on_curated")
             self.starting_life_count.set(1)
 
             self.requires_plando = "items"
@@ -547,7 +628,7 @@ class SuperMarioWorld(SuperMarioWorldBase):
         out:str = "\n\n"
         nam:str = self.mario_palette.get()
         
-        swp:float = float(self.number_of_yoshi_eggs.get()) * (float(self.percentage_of_yoshi_eggs.get()) / 100.0)
+        swp:float = float(self.max_yoshi_egg_cap.get()) * (float(self.percentage_of_yoshi_eggs.get()) / 100.0)
         egg:int = round(swp)
         
         
